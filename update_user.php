17f9515665836a57ebd7130f9c78613565595e88
Etienne PIERROT <etienne@leetchi.com>
2012-04-06 14:47:01 +0200

    add update user example

---
+++ update_user.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+	<body>
+		<pre>
+<?php
+
+require_once (dirname(__FILE__) . "/lib/common.inc");
+
+$user_id = isset($_REQUEST["user_id"]) ? $_REQUEST["user_id"] : 0;
+
+if ($user_id == 0) {
+	print("Error : not parameter user_id in url");
+	return;
+}
+
+/* we fetch the user with the user_id in the URL
+ * else we create the user
+ */
+
+/*
+ * GET to fetch the user
+ */
+$user = request("users/" . $user_id, "GET");
+if (!isset($user) || !isset($user -> ID)) {
+	print("Error");
+	return;
+}
+
+/*
+ * PUT request to update a user
+ */
+$body = json_encode(array("FirstName" => "Johnny", "LastName" => "Be Good", "Email" => "john.doe@unknow.com", "CanRegisterMeanOfPayment" => "false"));
+$user = request("users/".$user -> ID, "PUT", $body);
+?>
+
+</pre>
+	</body>
+</html>
