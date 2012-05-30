6299f865fea0283ee27e39c77e09afa192b3cac9
Etienne Pierrot <etienne.pierrot@leetchi.com>
2012-05-30 17:28:31 +0200

    example of usage of recurrent payment

--- return.php
+++ return.php
@@ -9,14 +9,22 @@
 require_once (dirname(__FILE__) . "/lib/common.inc");
 
 $contributionID = isset($_REQUEST["ContributionID"]) ? $_REQUEST["ContributionID"] : 0;
+$recurrentContributionId = isset($_REQUEST["RecurrentContributionId"]) ? $_REQUEST["RecurrentContributionId"] : 0;
 
-$contribution = request("contributions/$contributionID", "GET");
-
-$user = request("users/$contribution->UserID", "GET");
-
-if ($contribution -> WalletID != 0) {
+if( $contributionID != 0)
+{
+	$contribution = request("contributions/$contributionID", "GET");
 	$wallet = request("wallets/$contribution->WalletID", "GET");
 }
+
+if($recurrentContributionId != 0)
+{
+	$recurrentcontribution = request("recurrent-contributions/$recurrentContributionId", "GET");
+	$wallet = request("wallets	/$recurrentcontribution->WalletID", "GET");
+}
+
+
+
 ?>
 </pre>
 	</body>
