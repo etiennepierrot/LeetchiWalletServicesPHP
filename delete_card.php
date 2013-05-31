a765b8423491b12b47ccb68da51d68f87354cb51
Etienne Pierrot <etienne.pierrot@leetchi.com>
2013-05-31 11:09:02 +0200

    fix delete card

--- delete_card.php
+++ delete_card.php
@@ -5,7 +5,7 @@
 <body>
 <pre>
 <?php
-require_once(dirname(__FILE__) . "./common.inc");
+require_once(dirname(__FILE__) . "./lib/common.inc");
 
 $id_card = $_GET['paymentcard_id'];
 $response = request("cards/".$id_card, "DELETE");
